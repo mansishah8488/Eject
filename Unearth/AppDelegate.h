428c12d96b19fcc78e7f13b54946e0581527607b
Manasi <oab@OABs-Mac-mini-2.local>
2013-10-22 11:05:05 +0530

    Initial Commit

---
+++ Unearth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Unearth
+//
+//  Created by Manasi on 22/10/13.
+//  Copyright (c) 2013 OAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UnearthTests/UnearthTests.m
@@ -0,0 +1,34 @@
+//
+//  UnearthTests.m
+//  UnearthTests
+//
+//  Created by Manasi on 22/10/13.
+//  Copyright (c) 2013 OAB. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UnearthTests : XCTestCase
+
+@end
+
+@implementation UnearthTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
